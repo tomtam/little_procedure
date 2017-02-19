0458ee210216120117738eecea35899e942222c2
louzhiqiang <lzqwdd@sina.cn>
2017-02-19 22:50:14 +0800

    bug

--- campaign/modules/api/controllers/BaseController.php
+++ campaign/modules/api/controllers/BaseController.php
@@ -9,11 +9,13 @@ class BaseController extends ActiveController{
     public function beforeAction($action){
         $aesStr = Yii::$app->request->post('aesStr');
         if(!$aesStr){
+            Yii::info("----参数缺失:缺少aesStr-----", 'api');
             exit(Code::errorExit(Code::ERROR_PARAM_PARTIAL));
         }
         $params = Yii::$app->request->post();
         unset($params['aesStr']);
         if($aesStr != sha1(json_encode($params, JSON_UNESCAPED_UNICODE).Yii::$app->params['aes'])){
+            Yii::info("----aesStr校验不通过:aesStr参数：".$aesStr."-----php生成的：".sha1(json_encode($params, JSON_UNESCAPED_UNICODE).Yii::$app->params['aes']), 'api');
             exit(Code::errorExit(Code::ERROR_PARAM_CHECK));
         }
         return true;
