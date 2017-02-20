835517a75e800db2133109c7b8b761e92af47c8f
louzhiqiang <lzqwdd@sina.cn>
2017-02-20 21:53:56 +0800

    wzapi

--- campaign/components/Wxapi.php
+++ campaign/components/Wxapi.php
@@ -9,7 +9,7 @@ class Wxapi{
     const secretApi = "https://api.weixin.qq.com/sns/jscode2session?appid={__APPID__}&secret={__SECRET__}&js_code={__JSCODE__}&grant_type=authorization_code";
     
     public static function getSecretId($code){
-        $url = str_replace("{__CODE__}", $code, self::secretApi);
+        $url = str_replace("{__JSCODE__}", $code, self::secretApi);
         $url = str_replace("{__APPID__}", Yii::$app->params['appId'], $url);
         $url = str_replace("{__SECRET__}", Yii::$app->params['appSecret'], $url);
         
