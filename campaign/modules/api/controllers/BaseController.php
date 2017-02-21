6f439dc70447643db7746ade88c2320f579420f0
louzhiqiang <lzqwdd@sina.cn>
2017-02-21 22:39:52 +0800

    add log

--- campaign/modules/api/controllers/BaseController.php
+++ campaign/modules/api/controllers/BaseController.php
@@ -21,7 +21,7 @@ class BaseController extends ActiveController{
         }
         unset($params['aesStr']);
         Yii::info("-----params:".print_r($params, true), 'api');
-        if($aesStr != sha1(json_encode($params, JSON_UNESCAPED_UNICODE).Yii::$app->params['aes'])){
+        if($aesStr != sha1(json_encode($params, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES).Yii::$app->params['aes'])){
             Yii::info("----aesStr校验不通过:aesStr参数：".$aesStr."-----php生成的：".sha1(json_encode($params, JSON_UNESCAPED_UNICODE).Yii::$app->params['aes']), 'api');
             exit(Code::errorExit(Code::ERROR_PARAM_CHECK));
         }
