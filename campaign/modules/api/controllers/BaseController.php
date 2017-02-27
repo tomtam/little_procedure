d72d4833d86ebc6ad57a2184576681f4a1cd96c0
louzhiqiang <lzqwdd@sina.cn>
2017-02-27 13:44:21 +0800

    bug

--- campaign/modules/api/controllers/BaseController.php
+++ campaign/modules/api/controllers/BaseController.php
@@ -15,6 +15,9 @@ class BaseController extends ActiveController{
             exit(Code::errorExit(Code::ERROR_PARAM_PARTIAL));
         }
         $params = Yii::$app->request->post();
+        if(isset($params['locationName']) && $params['locationName']){
+            $params['locationName'] = explode(",", $params['locationName']);
+        }
         unset($params['aesStr']);
         Yii::info("-----params:".print_r($params, true), 'api');
         if($aesStr != sha1(json_encode($params, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES).Yii::$app->params['aes'])){
