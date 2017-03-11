d467e1c337e3a062733da6bd6deddd5b576d125a
louzhiqiang <lzqwdd@sina.cn>
2017-03-11 10:42:13 +0800

    alisms

--- campaign/modules/wap/controllers/UserController.php
+++ campaign/modules/wap/controllers/UserController.php
@@ -5,6 +5,7 @@ use Yii;
 use campaign\models\User;
 use campaign\components\Code;
 use campaign\models\Login;
+use campaign\components\Alisms;
 
 class UserController extends BaseController{
     public $modelClass = '';
@@ -88,7 +89,8 @@ class UserController extends BaseController{
     * @desc:   需要验证码
     */
     public function actionPhoneSend(){
-        $vcode = Yii::$app->request->post('vcode');
+        $vcode = Yii::$app->request->post('verifyCode');
+	Yii::info("---发送验证码借口，图形验证码：".$vcode, "api");
         if($vcode != Yii::$app->session[Login::VERIFY_CODE_SESSION_KEY]){
             return Code::errorExit(Code::ERROR_VERIFY_CHECK);
         }
@@ -98,6 +100,14 @@ class UserController extends BaseController{
         //发送手机验证码
         
         $code = '123456';
+	$accesskeyid = "LTAIzb9NF6L2AYtJ";
+	$accesskeysecret = "SEkvsn6qXntLYfq4PeQ19gVRdbVdAW";
+	$smstempcode = "SMS_50820106";
+	$params_code = "{'code':'".$code."'}";
+	$sms = new Alisms($accesskeyid, $accesskeysecret);
+	$resutn_arr = $sms->smsend($phone, $smstempcode, $params_code);
+
+	Yii::info("----sms接口返回数据：".print_r($return_arr, true), "api");
         Yii::$app->session[Login::PHONE_CODE_SESSION] = $code;
         Yii::$app->session[self::USER_WAP_REGISTER_STEP_ONE] = true;
         return Code::errorExit(Code::SUCC);
